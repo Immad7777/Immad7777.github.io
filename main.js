096ff0cd5419f200a09d5edc2e753c9b70f558e2
Immad7777 <157436942+Immad7777@users.noreply.github.com>
2024-02-26 18:44:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById("myBtn").onclick = function() {
     var name = prompt("Please enter your name:", "");
     if (name != null && name != "") {
-        document.getElementById("Heads").innerHTML = "Hello, " + name + "!";
+        document.getElementById("Heads").innerHTML = "Hello, " + name;
     }
 };
 
