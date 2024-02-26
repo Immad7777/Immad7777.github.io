d23c29cd0f9056edc0ff67f203634a49aa8ed4a7
Immad7777 <157436942+Immad7777@users.noreply.github.com>
2024-02-26 14:33:36 -0500

    Mouseover

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ document.getElementById("myBtn").onclick = function() {
 };
 
 
+document.getElementById("myDP").onclick = function() {
+    var img = this.getElementsByTagName('img')[0];
+    img.classList.toggle("Zoomed");
+}
+
+function highlight() {
+    this.getElementsById(Navbar);
+    Navbar.classList.toggle("Naviin");
+}
+
+function unhighlight() {
+    this.getElementsById(Navbar);
+    Navbar.classList.toggle("nav");
+}
