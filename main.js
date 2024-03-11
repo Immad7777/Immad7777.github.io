c23fbceff35e92bac264e267431f6f3574543d6d
Immad7777 <157436942+Immad7777@users.noreply.github.com>
2024-03-11 02:32:51 -0400

    trial

--- main.js
+++ main.js
@@ -22,15 +22,41 @@ function unhighlight() {
 }
 
 
-function redirectToPage() {
-    var selectedPage = document.getElementById("Drop_down").value;
-    if (selectedPage === "Home") {
-        window.location.href = "index.html";
-    } else if (selectedPage === "Travel") {
-        window.location.href = "#image.html"; // Change to the correct href for Travel page
-    } else if (selectedPage === "Experience") {
-        window.location.href = "#Experience"; // Change to the correct href for Experience page
-    } else if (selectedPage === "Future plans") {
-        window.location.href = "#Future"; // Change to the correct href for Future Plans page
+document.getElementById("myBtn").onclick = function() {
+    var name = prompt("Please enter your name:", "");
+    if (name != null && name != "") {
+        document.getElementById("Heads").innerHTML = "Hello, " + name;
     }
+};
+
+document.getElementById("myDP").onclick = function() {
+    var img = this.getElementsByTagName('img')[0];
+    img.classList.toggle("Zoomed");
+}
+
+function showDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = 'block';
+}
+
+function showDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = 'block';
+}
+
+// hide dropdown
+function hideDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = 'none';
+}
+
+function showDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = 'block';
+}
+
+// hide dropdown
+function hideDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = 'none';
 }
